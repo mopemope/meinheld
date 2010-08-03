54c207c0e897759ce32b8d100033ef9b1366841e
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2010-08-03 09:10:40 +0900

    change version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ include_dirs=[]
 
 
 setup(name='meinheld',
-    version="0.1.1",
+    version="0.1.2dev",
     description="High performance asynchronous Python WSGI Web Server",
     long_description=read('README.rst'),
     author='yutaka matsubara',
