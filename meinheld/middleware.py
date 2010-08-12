6db03952deb29b2a200c217be9433719dce22ae1
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2010-08-12 10:45:09 +0900

    add middleware

---
+++ meinheld/middleware.py
@@ -0,0 +1,27 @@
+import greenlet
+
+CONTINUATION_KEY = 'meinheld.continuation'
+
+class Continuation(object):
+
+    def __init__(self, cothread):
+        self._greenlet = cothread
+
+    def suspend(self):
+        parent = self._greenlet.parent
+        return parent.switch(-1)
+    
+    def resume(self):
+        self._greenlet
+
+class SpawnMiddleware(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        g = greenlet.greenlet(self.app)
+        c = Continuation(g)
+        environ[CONTINUATION_KEY] = c
+        return g.switch(environ, start_response)
+
