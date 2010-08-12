5a613ae68ab0a09d1823b699c76b4ccf7c579f11
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2010-08-12 15:50:16 +0900

    add resume callback

--- meinheld/middleware.py
+++ meinheld/middleware.py
@@ -1,18 +1,22 @@
+from meinheld import server
 import greenlet
 
+CLIENT_KEY = 'meinheld.client'
 CONTINUATION_KEY = 'meinheld.continuation'
 
 class Continuation(object):
 
-    def __init__(self, g):
+    def __init__(self, client, g):
         self._greenlet = g
+        self.client = client
 
     def suspend(self):
         parent = self._greenlet.parent
         return parent.switch(-1)
     
     def resume(self):
-        self._greenlet
+        server._resume_client(self.client)
+        
 
 class SpawnMiddleware(object):
 
@@ -21,7 +25,11 @@ class SpawnMiddleware(object):
 
     def __call__(self, environ, start_response):
         g = greenlet.greenlet(self.app)
-        c = Continuation(g)
+        
+        client = environ[CLIENT_KEY]
+        client.set_greenlet(g)
+
+        c = Continuation(client, g)
         environ[CONTINUATION_KEY] = c
         return g.switch(environ, start_response)
 
--- meinheld/server/client.h
+++ meinheld/server/client.h
@@ -2,15 +2,20 @@
 #define CLIENT_H
 
 #include "server.h"
+#include "greenlet.h"
 
 typedef struct {
     PyObject_HEAD
     client_t *client;
+    PyGreenlet *greenlet;
 } ClientObject;
 
 extern PyTypeObject ClientObjectType;
 
 inline PyObject* 
 ClientObject_New(client_t* client);
+
+inline void 
+setup_client(void);
 
 #endif
