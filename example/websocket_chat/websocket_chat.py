d50bfdf983aca84eb56486390dc87b6be7af584f
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2010-08-22 08:51:01 +0900

    mv websocket_chat example use flask

---
+++ example/websocket_chat/websocket_chat.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from meinheld import server, middleware
+
+SECRET_KEY = 'development key'
+DEBUG=True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+participants = set()
+
+
+@app.route('/')
+def index():
+    return render_template('websocket_chat.html')
+
+@app.route('/chat')
+def chat():
+    ws = request.environ.get('wsgi.websocket')
+    participants.add(ws)
+    try:
+        while True:
+            print "ws.wait()..."
+            m = ws.wait()
+            print "recv msg %s" % m
+            if m is None:
+                break
+            for p in participants:
+                print "send message %s" % m
+                a = p.send(m)
+                print "%s" % a
+    finally:
+        participants.remove(ws)
+    return ""
+
+        
+if __name__ == "__main__":
+    server.listen(("0.0.0.0", 8000))
+    server.run(middleware.WebSocketMiddleware(app))
+
+
+
