5af60bcc0d824ac8ed7567005b280da2c1cb353d
mopemope <yutaka.matsubara@gmail.com>
2010-08-22 18:20:10 +0900

    support access log to stdout

--- example/hello_world.py
+++ example/hello_world.py
@@ -9,7 +9,7 @@ def hello_world(environ, start_response):
     return [res]
 
 server.listen(("0.0.0.0", 8000))
-#server.access_log('/tmp/acc.log')
+server.access_log('stdout')
 #server.error_log('/tmp/err.log')
 server.run(hello_world)
 
