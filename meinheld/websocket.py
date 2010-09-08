753d2ff7f0273179d34d2e4db1544431aa746964
Yutaka Matsubara <yutaka.matsubara@gmail.com>
2010-09-08 09:11:42 +0900

    embed greenlet

--- meinheld/websocket.py
+++ meinheld/websocket.py
@@ -107,15 +107,10 @@ class WebSocketMiddleware(object):
 
     def __call__(self, environ, start_response):
         client = environ[CLIENT_KEY]
-        g = client.get_greenlet()
-        if not g:
-            # new greenlet
-            g = greenlet.greenlet(self.spawn_call)
-            client.set_greenlet(g)
-            c = Continuation(client)
-            environ[CONTINUATION_KEY] = c
+        c = Continuation(client)
+        environ[CONTINUATION_KEY] = c
 
-        return g.switch(environ, start_response)
+        return self.spawn_call(environ, start_response)
 
 class WebSocketWSGI(object):
 
