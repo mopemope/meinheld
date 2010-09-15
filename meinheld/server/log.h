d110535f989f0b556bae1002a7c6156450e9fffa
mopemope <yutaka.matsubara@gmail.com>
2010-09-15 21:24:42 +0900

    add inline

--- meinheld/server/log.h
+++ meinheld/server/log.h
@@ -4,13 +4,13 @@
 #include "client.h"
 #include "time_cache.h"
 
-int
+inline int
 open_log_file(const char *path);
 
-void
+inline void
 write_error_log(char *file_name, int line);
 
-int 
+inline int 
 write_access_log(client_t *cli, int log_fd, const char *log_path);
 
 #endif
