d61f02e46c50fcf835aa48f34853e2c21517fcf5
mopemope <yutaka.matsubara@gmail.com>
2010-10-20 10:56:31 +0900

    version ++

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ include_dirs=[]
 
 
 setup(name='meinheld',
-    version="0.4.6dev",
+    version="0.4.6",
     description="High performance asynchronous Python WSGI Web Server",
     long_description=read('README.rst'),
     author='yutaka matsubara',
