8097a2210efab82c768cbc47e22a1ab9254f685d
Ricardo Kagawa <ricardo.kagawa@gmail.com>
2014-09-24 00:21:06 -0300

    Fixed and improved autobahn test runner.

--- tests/autobahn_test.py
+++ tests/autobahn_test.py
@@ -1,20 +1,69 @@
-import logging
-import subprocess
+from os.path import join, abspath
+from subprocess import Popen, call
 import sys
+import meinheld
+
+
+def ensure_virtualenv():
+    if not hasattr(sys, 'real_prefix'):
+        raise Exception('Not inside a virtualenv')
+
+def ensure_python2():
+    src = '''
+import sys
+try:
+    sys.real_prefix
+    sys.exit(0)
+except AttributeError:
+    sys.exit(1)
+'''
+
+    if call(('python2', '-c', src)):
+        call(('virtualenv', '-p', 'python2', sys.prefix))
+
+    zipfile = 'meinheld-{}.zip'.format(meinheld.__version__)
+    zipfile = abspath(join(sys.prefix, '..', 'dist', zipfile))
+    call(('pip2', 'install', '--pre', '-U', zipfile))
+
+def ensure_wstest():
+    try:
+        call(('wstest', '-a'))
+    except FileNotFoundError:
+        call(('pip2', 'install', 'autobahntestsuite'))
+
+def setup_servers():
+    server27 = Popen(('python2', 'autobahn_test_servers.py', '8002'))
+    server34 = Popen(('python3', 'autobahn_test_servers.py', '8003'))
+    return server27, server34
+
+def teardown_servers(servers):
+    for server in servers:
+        try:
+            server.kill()
+            server.wait()
+        except AttributeError:
+            pass
+
+def runtests():
+    """Runs the Autobahn Test Suite against Meinheld on both Python 2.7 and
+    Python 3.4. The test suite itself should run on Python 2.7, and should
+    collect results from all servers from a single run (to report correctly).
+    This is why both servers are tested from a single environemnt.
+    """
+
+    ensure_virtualenv()
+    ensure_python2()
+    ensure_wstest()
+
+    status, server27, server34 = 1, False, False
+    try:
+        servers = setup_servers()
+        status = call(('wstest', '-m', 'fuzzingclient',
+                '-s', 'fuzzingclient.json'))
+    finally:
+        teardown_servers(servers)
+    sys.exit(status)
 
 if __name__ == '__main__':
-    server27 = subprocess.Popen(('python2', 'autobahn_test_servers.py',
-                               '8002'), cwd='tests')
-    server34 = subprocess.Popen(('python3', 'autobahn_test_servers.py',
-                               '8003'), cwd='tests')
-    client = subprocess.Popen(('wstest', '-m', 'fuzzingclient', '-s',
-                               'fuzzingclient.json'), cwd='tests')
-
-    status = client.wait()
-    server27.kill()
-    server34.kill()
-
-    server27.wait()
-    server34.wait()
-    sys.exit(status)
+    runtests()
 
--- tests/autobahn_test_servers.py
+++ tests/autobahn_test_servers.py
@@ -4,8 +4,16 @@ from meinheld import server
 from meinheld.middleware import WebSocketMiddleware
 
 def application(env, start_response):
-    start_response('404 Not Found', [])
-    return []
+    if env.get('wsgi.websocket') is not None:
+        ws = env.get('wsgi.websocket')
+        while True:
+            message = ws.wait()
+            if message is None:
+                return []
+            ws.send(message)
+    else:
+        start_response('404 Not Found', [])
+        return []
 
 if __name__ == '__main__':
     port = int(sys.argv[1] or '8000')
