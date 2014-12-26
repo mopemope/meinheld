c47cdba7a1fe6a80dc8a4f9f2ac5288104f1102f
INADA Naoki <songofacandy@gmail.com>
2014-12-26 22:27:28 +0900

    Update bench examples.

--- bench/hello/bjoern_server.py
+++ bench/hello/bjoern_server.py
@@ -2,7 +2,7 @@ import bjoern
 
 def hello_world(environ, start_response):
     status = '200 OK'
-    res = "Hello world!"
+    res = b"Hello world!"
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
     return [res]
--- bench/hello/meinheld_server.py
+++ bench/hello/meinheld_server.py
@@ -1,7 +1,7 @@
 from meinheld import server
 
 def hello_world(environ, start_response):
-    status = b'200 OK'
+    status = '200 OK'
     res = b"Hello world!"
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
@@ -11,4 +11,3 @@ server.listen(("0.0.0.0", 8000))
 server.set_access_logger(None)
 server.set_error_logger(None)
 server.run(hello_world)
-
