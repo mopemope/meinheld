bccb71d275dba5365efb0e914435b1d56179a087
Igor Petrik <ipetrik@ambrygen.com>
2019-11-01 10:21:00 -0700

    Changes NATIVE_ASSTRING for PY2 & 3 compatability

--- meinheld/server/server.c
+++ meinheld/server/server.c
@@ -857,7 +857,7 @@ check_http_expect(client_t *client)
         ///TODO CHECK
         c = PyDict_GetItemString(req->environ, "HTTP_EXPECT");
         if (c) {
-            val = PyUnicode_AsUTF8(c);
+            val = NATIVE_ASSTRING(c);
             if (!strncasecmp(val, "100-continue", 12)) {
                 ret = write(client->fd, "HTTP/1.1 100 Continue\r\n\r\n", 25);
                 if (ret < 0) {
